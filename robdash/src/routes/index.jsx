4ffa63ed943e33d460160e85630bb04b5389ba90
Felipe Mayer <mayerdevelopment@outlook.com.br>
2022-08-16 23:48:14 -0300

    delete

--- robdash/src/routes/index.jsx
+++ robdash/src/routes/index.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Login } from "../pages/Login";
-import { Register } from "../pages/Register";
 import { Home } from "../pages/Home";
 import { PrivateRoute } from "./privateRoutes";
 
@@ -10,8 +9,6 @@ export const AppRouter = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
         <Route path="/home" element={<PrivateRoute />}>
           <Route path="/home" element={<Home />} />
         </Route>
