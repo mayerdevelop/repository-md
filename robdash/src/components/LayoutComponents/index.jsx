2a45c0a648ad0dd6e8a30288a766b7deb8d9b289
Felipe Mayer <mayerdevelopment@outlook.com.br>
2022-08-20 13:37:14 -0300

    delete

--- robdash/src/components/LayoutComponents/index.jsx
+++
@@ -1,8 +0,0 @@
-import "./styles.css";
-export const LayoutComponents = (props) => {
-  return (
-    <div className="container">
-      {props.children}
-    </div>
-  );
-};
--- robdash/src/services/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "http://200.98.81.201:40160/rest",
-});
